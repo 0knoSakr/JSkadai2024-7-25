7b3d8ad1d1494109ae1eb2fdeb1e9b5428e5dd84
okano <s.okano@ebacorp.jp>
2024-07-25 18:59:38 +0900

    ログイン画面のスクリプトjsファイルの作成

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//ログイン画面
+let logIn = document.getElementsByClassName("button"):
+  button.addEventListener("click", function () {
+    if (!value.match(/^[A-Za-z0-9]*$/)) {
+      error = '半角英数字のみで入力してください。'
+}
+  });
