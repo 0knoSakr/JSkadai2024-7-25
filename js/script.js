f21fdea57c6853ea42f296b6425b52ebffa581b7
okano <s.okano@ebacorp.jp>
2024-07-26 11:45:21 +0900

    id項目のチェックjsの追加

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 //ログイン画面
-let logIn = document.getElementsByClassName("button"):
-  button.addEventListener("click", function () {
-    if (!value.match(/^[A-Za-z0-9]*$/)) {
-      error = '半角英数字のみで入力してください。'
+function subForm() {
+  //正規表現　半角英数字記号のみ入力可能の変数指定
+  let pattern = /^[a-zA-Z0-9!-/:-@¥[-`{-~]+$/;
+  let account = document.getElementById("accId").value;
+  let password = document.getElementById("pswId").value;
+  let isRight = true;
+  //ID項目のチェック
+  if (pattern.text(account)) {
+    document.getElementById("spId").innerHTML = "";
+  } else {
+    document.getElementById("spPs").innerHTML = "IDは半角英数字記号のみで記入して下さい";
+    isRight = false;
+  }
+  //パスワード項目のチェック
 }
-  });
