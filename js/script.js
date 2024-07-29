72caccde556676b4b66fc4bc85207a918daa04f2
okano <s.okano@ebacorp.jp>
2024-07-29 12:51:20 +0900

    バリデーション動作の追加２

--- js/script.js
+++ js/script.js
@@ -1,42 +1,55 @@
 //ログイン画面
-function subForm() {
-  //正規表現　半角英数字記号のみ入力可能の変数指定
-  let pattern = /^[a-zA-Z0-9!-/:-@¥[-`{-~]*$/;
+//正規表現　半角英数字記号のみ入力可能の変数指定
+let pattern = /^[a-zA-Z0-9!-/:-@¥[-`{-~]*$/;
+let button = document.getElementById("button");
+//divの情報を持ってきてる
+let spId = document.getElementById("spId");
+let spPs = document.getElementById("spPs");
+let isRight = true;
+//ボタンをクリックしたらvalueを読み込んで検証する
+button.addEventListener("click", function () {
+  //textBoxの情報を持ってくる
   let account = document.getElementById("accId").value;
-  let password = document.getElementById("pswId").value;
-  let isRight = true;
+  let password = document.getElementById("paswId").value;
+  let error1 = document.getElementById("error1");
+  let error2 = document.getElementById("error2");
+
   //ID項目のチェック
-  if (pattern.text(account)) {
-    document.getElementById("spId").innerHTML = "";
-    console.log(account);
+  if (pattern.test(account)) {
+    error1.innerHTML = "";
   } else {
-    document.getElementById("spId").innerHTML ="IDは半角英数字記号のみで記入して下さい";
+    // let text = `<p>IDは半角英数字記号のみで記入して下さい</p>`;
+    error1.innerHTML = "IDは半角英数字記号のみで記入して下さい";
     isRight = false;
+    // console.log("テスト")
   }
   //パスワード項目のチェック
-  if (pattern.text(password)) {
-    document.getElementById("spPs").innerHTML = "";
+  if (pattern.test(password)) {
+    error2.innerHTML = "";
   } else {
-    document.getElementById("spPs").innerHTML ="IDは半角英数字記号のみで記入して下さい";
+    // let text = `<p>IDは半角英数字記号のみで記入して下さい</p>`;
+    error2.innerHTML = "IDは半角英数字記号のみで記入して下さい";
     isRight = false;
+    console.log("テスト")
   }
+
+  //フォームボタンを押してもページが変わらなくなる
   let form = document.getElementById("form");
 
-  let submitButton = document.getElementsByClassName("button");
-
-  submitButton.onclick = () => {
+  button.onclick = () => {
     let formData = new FormData(form);
     let action = form.getAttribute("action");
     let option = {
       method: "POST",
       body: formData,
     };
-    fetch(action, options).then((e) => {
-      if (e.status === 200) {
-        alert("保存しました。");
-        return;
-      }
-      alert("保存できませんでした。");
-    });
+
+    // fetch(action, options).then((e) => {
+    //   if (e.status === 200) {
+    //     alert("保存しました。");
+    //     return;
+    //   }
+    //   alert("保存できませんでした。");
+    // });
   };
-}
+});
